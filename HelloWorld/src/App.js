043502c335ab70082d676ccb5afac4d2f96b4d58
hsinhan <bmhiamso1@gmail.com>
2017-03-12 19:09:46 +0800

    extract configureScene  function

--- HelloWorld/src/App.js
+++ HelloWorld/src/App.js
@@ -3,11 +3,16 @@ import { View, Text, Navigator } from 'react-native'
 
 import Page1 from './Pages/Page1'
 class App extends Component {
+  _configureScene = ( route, routeStack ) => {
+    return Navigator.SceneConfigs.VerticalDownSwipeJump;
+    // return Navigator.SceneConfigs.FloatFromBottom
+    // return Navigator.SceneConfigs.PushFromRight
+  };
     render() {
         return (
             <Navigator
                 initialRoute={{component: Page1}}
-                configureScene={(route)=> {return Navigator.SceneConfigs.VerticalDownSwipeJump;}}
+                configureScene={ this._configureScene }
                 renderScene={(route, navigator)=> {
                     let Component = route.component;
                     return <Component  navigator={navigator}/> 
